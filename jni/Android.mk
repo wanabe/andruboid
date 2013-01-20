c9a2bc773d71157a67796e054c54a5aeb5c4a831
wanabe <s.wanabe@gmail.com>
2013-01-20 17:12:12 +0900

    update jni/Android.mk's dependencies. need re-compile if mruby is updated.

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,12 +2,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := andruboid
 LOCAL_C_INCLUDES := jni/mruby/include jni/mruby/src
-LOCAL_SRC_FILES := andruboid.c app.c \
-		   mruby/build/host/mrblib/mrblib.c \
-		   mruby/build/host/src/y.tab.c \
-		   $(patsubst jni/%, %, $(wildcard jni/mruby/src/*.c))
+MRUBY_SRC_FILES := jni/mruby/build/host/src/y.tab.c \
+		   jni/mruby/build/host/mrblib/mrblib.c \
+		   $(wildcard jni/mruby/src/*.c)
+LOCAL_SRC_FILES := andruboid.c app.c mruby/build/host/mrblib/mrblib.c \
+		   $(patsubst jni/%, %, $(MRUBY_SRC_FILES))
+
+jni/mruby/bin/mrbc: $(MRUBY_SRC_FILES)
+
 jni/mruby/build/host/mrblib/mrblib.c:
 	cd jni/mruby; make
-jni/%.c : jni/%.rb
+
+jni/%.c : jni/%.rb jni/mruby/bin/mrbc
 	cd jni; mruby/bin/mrbc -B$* $*.rb
+
 include $(BUILD_SHARED_LIBRARY)
