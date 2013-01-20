9e86b4a2845e9ea4af84ac521270c3601d224b54
wanabe <s.wanabe@gmail.com>
2013-01-20 21:54:02 +0900

    update Android.mk for xtaak and *.cc

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,11 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := andruboid
-LOCAL_C_INCLUDES := jni/mruby/include jni/mruby/src
+LOCAL_C_INCLUDES := jni/mruby/include jni/mruby/include/xtaak jni/mruby/src
 MRUBY_SRC_FILES := jni/mruby/build/host/src/y.tab.c \
 		   jni/mruby/build/host/mrblib/mrblib.c \
-		   $(wildcard jni/mruby/src/*.c)
-LOCAL_SRC_FILES := andruboid.c app.c mruby/build/host/mrblib/mrblib.c \
+		   $(wildcard jni/mruby/src/*.c)\
+		   $(wildcard jni/mruby/src/*.cc)
+LOCAL_SRC_FILES := andruboid.c app.c \
 		   $(patsubst jni/%, %, $(MRUBY_SRC_FILES))
 
 jni/mruby/bin/mrbc: $(MRUBY_SRC_FILES)
